bef85540fe9d20b8cb64c1e9a35515f9c152f1af
Mark D <duistersmark@hotmail.com>
2025-10-27 23:35:05 +0100

    update examen voorbeeld.

--- Assets/MyAssets/Scripts/SuperRealisticCandle.cs
+++ Assets/MyAssets/Scripts/SuperRealisticCandle.cs
@@ -5,22 +5,44 @@ public class SuperRealisticCandle : MonoBehaviour
 
     Light getLight => GetComponent<Light>();
     float originalIntensity = 1f;
+    float newIntensity;
+
+    Vector3 originalPosition;
+    Vector3 newPosition;
     float counter = 0f;
     [SerializeField] float updateRate = 0.1f;
+    [SerializeField] float maxDisplacement = 0.01f;
+    [SerializeField] float interpolationSpeed = 10f;
 
     void Start()
     {
         originalIntensity = getLight.intensity;
+        originalPosition = transform.position;
+         newIntensity = originalIntensity;
+        newPosition = originalPosition;
     }
     void Update()
     {
 
         counter += Time.deltaTime;
 
+        getLight.intensity = Mathf.Lerp(getLight.intensity, newIntensity, Time.deltaTime*interpolationSpeed);
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime*interpolationSpeed);
+
         if (counter >= updateRate)
         {
-            getLight.intensity = Random.Range(0.025f, originalIntensity);
+            newIntensity = Random.Range(0.025f, originalIntensity);
+            newPosition = originalPosition + RandomPosition();
             counter = 0f;
         }
+    }
+
+    Vector3 RandomPosition()
+    {
+        float x = Random.Range(0f, maxDisplacement),
+         y = Random.Range(0f, maxDisplacement),
+          z = Random.Range(0f, maxDisplacement);
+
+        return new Vector3(x, y, z);
     }
 }
